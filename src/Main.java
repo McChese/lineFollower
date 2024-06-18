64a29936ba787961e9b99135f52cf1d78efa17f1
Michael <85956282+McChese@users.noreply.github.com>
2024-06-18 09:41:02 +0200

    idk anymore

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
 import lejos.hardware.ev3.LocalEV3;
-
-import javax.swing.*;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.robotics.Color;
 
 public class Main {
+
+    private static GraphicsLCD lcd = LocalEV3.get().getGraphicsLCD();
+    private static int wiggleAttempts = 0;
+
     public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Starting...");
-        Actions.followLine();
-
-        if (LocalEV3.get().getKey("Escape").isDown())
-        {
-            Actions.stopFollowLine();
+        while (true) {
+            if (Actions.getLEFTColorID() == Color.BLACK && Actions.getRIGHTColorID() == Color.BLACK) {
+                Actions.forward(300);
+            } else if (Actions.getLEFTColorID() == Color.BLACK && Actions.getRIGHTColorID() != Color.BLACK) {
+                Actions.turnLeft(200);
+            } else if (Actions.getLEFTColorID() != Color.BLACK && Actions.getRIGHTColorID() == Color.BLACK) {
+                Actions.turnRight(200);
+            } else {
+                try {
+                    wiggleAttempts++;
+                    Actions.wiggle();
+                    if (wiggleAttempts >= 4) {
+                        Actions.findLineWithGyro();
+                        wiggleAttempts = 0;
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-
     }
 }
